becea3554cdd936e96aceb380e0bc622e01bf098
acruxzx <juanfelipeladino8@gmail.com>
2025-04-23 16:17:15 -0500

    config database

--- config/database.php
+++ config/database.php
@@ -42,24 +42,21 @@ return [
             'synchronous' => null,
         ],
 
-       'mysql' => [
+     'mysql' => [
     'driver' => 'mysql',
-    'host' => env('DB_HOST', '127.0.0.1'),
-    'port' => env('DB_PORT', '3306'),
-    'database' => env('DB_DATABASE', 'laravel'),
-    'username' => env('DB_USERNAME', 'root'),
-    'password' => env('DB_PASSWORD', ''),
+    'url' => env('DATABASE_URL'), // Aquí tomamos la URL de la variable de entorno
+    'host' => parse_url(env('DATABASE_URL'))['host'],
+    'port' => parse_url(env('DATABASE_URL'))['port'],
+    'database' => ltrim(parse_url(env('DATABASE_URL'))['path'], '/'),
+    'username' => parse_url(env('DATABASE_URL'))['user'],
+    'password' => parse_url(env('DATABASE_URL'))['pass'],
     'unix_socket' => env('DB_SOCKET', ''),
-    'charset' => env('DB_CHARSET', 'utf8mb4'),
-    'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-    'prefix' => '',
-    'prefix_indexes' => true,
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
     'strict' => true,
     'engine' => null,
-    'options' => extension_loaded('pdo_mysql') ? array_filter([
-        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-    ]) : [],
 ],
+
 
 
         'mariadb' => [
